e690d3f4b6376d54b2b4bae6e1b64296e248797a
ilyes benmalti <benmaltiilyes@gmail.com>
2025-12-27 08:25:11 +0100

    feat: Add mongodb client and better-auth setups

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
+import clientPromise from "./mongodb";
+
+const client = await clientPromise;
+const db = client.db("drag-todos");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
